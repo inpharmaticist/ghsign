3b441efbd7fac5e1f25b2ed02dadddc976db97c0
inpharmaticist <cedwards@healthdatawise.com>
2022-11-01 16:38:29 -0700

    11012022163829

--- sign.sh
+++ sign.sh
@@ -2,16 +2,30 @@
 
 #The two lines below are the local and external addresses synced by a cloud service (currently formatted for Keybase on a linux machine).
 local=~/Desktop/signgit
-interweb=https://raw.githubusercontent.com/inpharmaticist/pysign/main/
+interweb=https://raw.githubusercontent.com/inpharmaticist/ghsign/main
 
 #Script
-cd Desktop
+#cd ghsign
 echo 'What message would you like to sign? (Type "|" aka pipe when done)
 '
 read -d'|' userentry
 
 datetime=`date +"%m%d%Y%H%M%S"`
 echo "$userentry"|gpg --clearsign > $datetime.txt
+
+#echo "$userentry" | gpg --clearsign | nc termbin.com 9999
+
+git add .
+wait
+git commit -m "$datetime"
+wait
+git push origin main
+wait
+inpharmaticist
+wait
+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
+wait
+
 
 echo "
 Here is your message:
